e2c4f75f087aab125dfd44ddf71a888359d937d7
NicollasGLV <galvaodosantosnicollas@gmail.com>
2026-02-28 21:57:54 -0300

    Excluir enviar.php

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nome = trim($_POST["nome"]);
-$msg = trim($_POST["msg"]);
-
-if($nome != "" && $msg != ""){
-    $linha = "<b>".htmlspecialchars($nome).":</b> ".htmlspecialchars($msg)."<br>\n";
-    file_put_contents("chat.txt",$linha,FILE_APPEND | LOCK_EX);
-}
-?>
