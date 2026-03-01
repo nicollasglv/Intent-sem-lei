9890176a48b0755e8768d4c1f88c592e78ec398b
NicollasGLV <galvaodosantosnicollas@gmail.com>
2026-02-28 21:24:31 -0300

    Atualizar o enviar.php

--- enviar.php
+++ enviar.php
@@ -1,9 +1,9 @@
 <?php
-$nome = $_POST["nome"];
-$msg = $_POST["msg"];
+$nome = trim($_POST["nome"]);
+$msg = trim($_POST["msg"]);
 
 if($nome != "" && $msg != ""){
-    $linha = "<b>".htmlspecialchars($nome).":</b> ".htmlspecialchars($msg)." <br>\n";
-    file_put_contents("chat.txt",$linha,FILE_APPEND);
+    $linha = "<b>".htmlspecialchars($nome).":</b> ".htmlspecialchars($msg)."<br>\n";
+    file_put_contents("chat.txt",$linha,FILE_APPEND | LOCK_EX);
 }
 ?>
